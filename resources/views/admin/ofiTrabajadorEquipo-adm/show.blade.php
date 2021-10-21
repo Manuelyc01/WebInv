61c8269cc4b6a3cd4facedd0b50a0e0a02b3f54d
unknown <manuelyanez01@outlook.com>
2021-10-21 09:29:30 -0500

    pdf dennis

--- resources/views/admin/ofiTrabajadorEquipo-adm/show.blade.php
+++ resources/views/admin/ofiTrabajadorEquipo-adm/show.blade.php
@@ -8,7 +8,7 @@
     <div class="panel panel-white ui-sortable-handle" style="opacity: 1;">
     <div class="panel-heading">
         <h3 class="panel-title form-title"> Equipo Asignado </h3>
-        @echo({{ public_path()}})
+        
         <div class="panel-control">
             <a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title="" class="panel-collapse" data-original-title="Expand/Collapse"><i class="icon-arrow-down"></i></a>
         </div>
