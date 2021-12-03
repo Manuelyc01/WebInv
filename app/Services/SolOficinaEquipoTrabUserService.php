5313d3e4280577e4a9efbc25368a8558657634db
unknown <manuelyanez01@outlook.com>
2021-12-03 10:46:03 -0500

    css 01/12

--- app/Services/SolOficinaEquipoTrabUserService.php
+++ app/Services/SolOficinaEquipoTrabUserService.php
@@ -133,6 +133,7 @@ class SolOficinaEquipoTrabUserService
 
     public function mostrar($id)
     {  
+        dd($id);
         $element = SolOficinaEquipoTrabUser::join('tm_solicitudes','tm_solicitudes.id_solicitud','=','tm_soli_ofi_equi_traba.id_solicitud')
                                         ->select('tm_soli_ofi_equi_traba.*','tm_solicitudes.*')
                                         ->where('tm_soli_ofi_equi_traba.id_soli_ofi_equi_tra',$id)
